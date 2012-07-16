cefa13a69a6413814374cd7e39e62991537350f7
Josiah Ruddell <jruddell@gmail.com>
2012-07-16 09:40:14 -0500

    init poc

---
+++ src/DependencyManagement.Core/Repositories/HelloWorldRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DependencyManagement.Core.Repositories
+{
+	class HelloWorldRepo
+	{
+		public string SayMyName()
+		{
+			return "Josiah Ruddell";
+		}
+	}
+}
